468e00c1fa0251de1c01dbcecac9344c9702fb07
AnilReddy-Gade <anilkumarreddyg000@gmail.com>
2025-07-29 22:16:59 +0530

    used USESTATE HOOK and created filter button for top restaurants

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,32 @@
-import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+import resList from "../utils/mockData";
+
 const Body = () => {
+  const [listOfRestaurants, setListOfRestraunt] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestraunt(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-card-container">
-        {/* restaurant cards */}
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
   );
 };
-export default Body
+
+export default Body;
